83c8ad8e27bb4b11f70bb01b2716a57e044d5e60
nishatvpn <92996099+nishatvpn@users.noreply.github.com>
2022-07-01 20:46:41 +0200

    Update ubuntu_ovpnws.sh

--- ubuntu_ovpnws.sh
+++ ubuntu_ovpnws.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cp /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 #Database Details
-HOST='209.159.152.66';
-USER='crypticv_freedata';
-PASS='@@@@F1r3n3t';
-DBNAME='crypticv_freedata';
+HOST='174.138.183.243';
+USER='abolirez_sagorb';
+PASS='abolirez_sagorb';
+DBNAME='abolirez_sagorb';
 
 install_require()
 {
